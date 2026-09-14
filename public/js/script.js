d1c02340694fedfc26397facded9bfb0991b61c2
Alex A <sashaaslanyan1903@gmail.com>
2026-09-14 15:19:48 +0300

    feat: implement admin routes and views for login and registration

--- public/js/script.js
+++ public/js/script.js
@@ -3,18 +3,23 @@ document.addEventListener('DOMContentLoaded', function(){
   const searchBar = document.querySelector('.searchBar');
   const searchInput = document.querySelector('#searchInput');
   const searchClose = document.querySelector('#searchClose');
-  for (let i = 0; i< allButtons.length; i++) {
-    allButtons[i].addEventListener('click', function(){
-      searchBar.style.visibility = 'visible';
-      searchBar.classList.add('open');
-      this.setAttribute('aria-expanded', 'true');
-      searchInput.focus();
-    });
+
+  if (searchBar && searchInput) {
+    for (let i = 0; i< allButtons.length; i++) {
+      allButtons[i].addEventListener('click', function(){
+        searchBar.style.visibility = 'visible';
+        searchBar.classList.add('open');
+        this.setAttribute('aria-expanded', 'true');
+        searchInput.focus();
+      });
+    }
   }
 
-  searchClose.addEventListener('click', function(){
-    searchBar.style.visibility = 'hidden';
-    searchBar.classList.remove('open');
-    this.setAttribute('aria-expanded', 'false');
-  });
+  if (searchClose && searchBar) {
+    searchClose.addEventListener('click', function(){
+      searchBar.style.visibility = 'hidden';
+      searchBar.classList.remove('open');
+      this.setAttribute('aria-expanded', 'false');
+    });
+  }
 })
